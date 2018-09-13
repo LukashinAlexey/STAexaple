5d1da231f31367ef8f0e47075f9eb8d1509397f5
Алексей <@DESKTOP-5VD06MM>
2018-09-13 21:31:42 +0300

    Добавьте файлы проекта.

---
+++ example/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	int nombersCount = 0;
+	int *  arr = new int[5];
+	while (true)
+	{
+		int temp;
+		std::cin >> temp;
+
+		if (std::cin)
+		{
+			arr[nombersCount++] = temp;
+
+		}
+		else
+			break;
+		
+	}
+	for (int i = nombersCount - 1; i >= 0; i--)
+	{
+		std::cout << arr[i] << ' ';
+	}
+	delete[] arr;
+	system("pause");
+	return 0;
+}
